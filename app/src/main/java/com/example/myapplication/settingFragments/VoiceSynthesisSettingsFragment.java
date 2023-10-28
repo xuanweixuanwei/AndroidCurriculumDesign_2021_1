ba1104b8289d2a620074d349f474ce7ed791e7da
xuanweixuanwei <1322300337@qq.com>
2023-10-28 22:14:19 +0800

    格式化，修改了一些写法（例如Timber），把UI操作尽量都写在专门的UI线程中，数据库操作在线程池中进行

--- app/src/main/java/com/example/myapplication/settingFragments/VoiceSynthesisSettingsFragment.java
+++ app/src/main/java/com/example/myapplication/settingFragments/VoiceSynthesisSettingsFragment.java
@@ -3,22 +3,25 @@ package com.example.myapplication.settingFragments;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.InputType;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.myapplication.R;
+
 import com.example.myapplication.util.InputFilterMinMax;
 
 public class VoiceSynthesisSettingsFragment extends PreferenceFragmentCompat {
 
-    private final static VoiceSynthesisSettingsFragment instance =
-            new VoiceSynthesisSettingsFragment();
+    private final static VoiceSynthesisSettingsFragment instance = new VoiceSynthesisSettingsFragment();
 
     private androidx.preference.EditTextPreference mSpeedPreference;
     private androidx.preference.EditTextPreference mPitchPreference;
     private androidx.preference.EditTextPreference mVolumePreference;
 
-    public static VoiceSynthesisSettingsFragment getInstance() {
+    public static VoiceSynthesisSettingsFragment getInstance(){
         return instance;
     }
 
@@ -26,30 +29,42 @@ public class VoiceSynthesisSettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences_voice_synthesis, rootKey);
 
-        mSpeedPreference = findPreference("speed_preference");
-        mPitchPreference = findPreference("pitch_preference");
-        mVolumePreference = findPreference("volume_preference");
+        mSpeedPreference = (EditTextPreference)findPreference("speed_preference");
+        mPitchPreference = (EditTextPreference) findPreference("pitch_preference");
+        mVolumePreference = (EditTextPreference) findPreference("volume_preference");
 
         setEditListener();
+
+
     }
 
     private void setEditListener() {
         assert mSpeedPreference != null;
-        mSpeedPreference.setOnBindEditTextListener(editText -> {
-            editText.setInputType(InputType.TYPE_CLASS_NUMBER);
-            editText.setFilters(new InputFilter[]{new InputFilterMinMax(0, 200)});
+        mSpeedPreference.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+            @Override
+            public void onBindEditText(@NonNull EditText editText) {
+                editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                editText.setFilters(new InputFilter[]{new InputFilterMinMax(0, 200)});
+            }
         });
 
         assert mPitchPreference != null;
-        mPitchPreference.setOnBindEditTextListener(editText -> {
-            editText.setInputType(InputType.TYPE_CLASS_NUMBER);
-            editText.setFilters(new InputFilter[]{new InputFilterMinMax(0, 100)});
+        mPitchPreference.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+            @Override
+            public void onBindEditText(@NonNull EditText editText) {
+                editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                editText.setFilters(new InputFilter[]{new InputFilterMinMax(0, 100)});
+            }
         });
 
         assert mVolumePreference != null;
-        mVolumePreference.setOnBindEditTextListener(editText -> {
-            editText.setInputType(InputType.TYPE_CLASS_NUMBER);
-            editText.setFilters(new InputFilter[]{new InputFilterMinMax(0, 100)});
+        mVolumePreference.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+            @Override
+            public void onBindEditText(@NonNull EditText editText) {
+                editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                editText.setFilters(new InputFilter[]{new InputFilterMinMax(0, 100)});
+            }
         });
+
     }
 }
